40fd394724d550f223303022c33a6ac56bb0208d
Rimavig <rvivanco@espol.edu.ec>
2020-06-06 13:24:48 -0500

    sadda

--- php/mensaje.php
+++
@@ -1,8 +0,0 @@
-<span class="error">
-  <div class="container1" id="container1" >
-    <div class="col3">
-        <h1>Error de Permiso</h1>
-        <p>Contactarse con el Administrador </p>
-    </div>
-  </div>
-</span>
