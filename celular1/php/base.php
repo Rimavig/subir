d9eddbe4ab1efc457da247e507f64e86ad07070a
Rimavig <rvivanco@espol.edu.ec>
2020-10-11 17:55:08 -0500

    sadas
    dasdas

--- celular1/php/base.php
+++
@@ -1,9 +0,0 @@
-<!--MENU PRINCIPAL-->
-<span class="error">
-    <div class="container1" id="container1" >
-    <div class="Image_container">
-        <img src="images\logo.png" alt="Avatar" class="logoB">
-    </div>
-  </div>
- </span>
-</div>
