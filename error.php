5f96a0e1f86488da9551d0b3a6010a84a0d0c0ed
Rimavig <rvivanco@espol.edu.ec>
2022-03-10 16:41:17 -0500

    gg

---
+++ error.php
@@ -0,0 +1,40 @@
+<?php
+  //MENSAJE
+  $status_registro="Se registro usuario correctamente";
+  $status_clave="Se cambio contraseña correctamente";
+  $status_reseteo="Se reseteo contraseña correctamente";
+  $status_categorias="Se reseteo contraseña correctamente";
+  //ERROR
+  $error_100_mensaje="Error con Servidor";
+  $error_100_causa="Credenciales invalidas";
+
+  $error_101_mensaje="Error de Autenticación";
+  $error_101_causa="Credenciales invalidas";
+
+  $error_102_mensaje="Error con base de datos";
+  $error_102_causa="Consulta invalida";
+  //login
+  $error_201_mensaje="Ingrese todos los parámetros";
+  $error_201_causa="Error de Parámetros";
+
+  $error_202_mensaje="Usuario no registrado";
+  $error_202_causa="Error de Autenticación";
+
+  $error_203_mensaje="Contraseña incorrecta";
+  $error_203_causa="Error de Autenticación";
+
+  $error_204_mensaje="El registro ya existe";
+  $error_204_causa="Error de registro";
+
+  $error_205_mensaje="Código caducado";
+  $error_205_causa="Error de código";
+
+  $error_206_mensaje="Código incorrecto";
+  $error_206_causa="Error de código";
+
+  $error_207_mensaje="No existen categorías activas";
+  $error_207_causa="Error de categoría";
+
+  $error_208_mensaje="No existe contacto ";
+  $error_208_causa="Error de contacto ";
+?>
