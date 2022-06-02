95395dc616eda5ed98c89ecff177b7e7657df4c1
Rimavig <rvivanco@espol.edu.ec>
2022-06-02 01:44:58 -0500

    fffff

--- TSA-Frontend/TSA/layout1/angularjs/app/directorio.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $path_imagen="/teatro-copia/imagenes/";
-  //$path_imagen1='/teatro-copia/imagenes/';
-  $path_imagen1='C:\xampp\htdocs\teatro-copia\imagenes/';
-  $path_imagen2='../../../assets/global/images/';
-  
-?>
