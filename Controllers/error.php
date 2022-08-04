5b80c4d56eae784660a1eb9194b4b193136f069c
Rimavig <rvivanco@espol.edu.ec>
2022-08-04 00:21:50 -0500

    gg

---
+++ Controllers/error.php
@@ -0,0 +1,118 @@
+<?php
+  //MENSAJE
+  $status_registro1="El usuario y correo no existen";
+  $status_registro="Se registro usuario correctamente";
+  $status_registroE="Se registro al evento correctamente";
+  $status_usuario="Se actualizó usuario correctamente";
+  $status_clave="Se cambio contraseña correctamente";
+  $status_asiento="Se reservó asiento correctamente";
+  $status_Dasiento="Se eliminó asiento correctamente";
+  $status_reseteo="Se reseteó contraseña correctamente";
+  $status_categorias="Se reseteo contraseña correctamente";
+  $status_delete="Se eliminó correctamente";
+  $status_facturacion="Se creo correctamente el registro de facturación";
+  $status_Ufacturacion="Se modificó correctamente el registro de facturación";
+  $status_CfacturacionF="Se creo correctamente el registro de facturación favorita";
+  $status_UfacturacionF="Se actualizó correctamente el registro de facturación favorita";
+  $status_CTarjetaF="Se creo correctamente  la tarjeta favorita";
+  $status_UTarjetaF="Se actualizó correctamente la tarjeta favorita";
+  $status_asientoE="Se actualizó correctamente";
+  $status_enviar="Se envió correctamente";
+  $ruta_logo= "http://104.198.222.134/imagenes/logo/";
+  $ruta_evento= "http://104.198.222.134/imagenes/evento/";
+  $ruta_distribucion= "http://104.198.222.134/imagenes/distribucion/";
+  $ruta_compartir= "http://104.198.222.134/evento/";
+  //ERROR
+  $error_100_mensaje="Error con Servidor";
+  $error_100_causa="Credenciales invalidas";
+
+  $error_101_mensaje="Error de Autenticación";
+  $error_101_causa="Credenciales invalidas";
+
+  $error_102_mensaje="Error con base de datos";
+  $error_102_causa="Consulta invalida";
+  //login
+  $error_201_mensaje="Ingrese todos los parámetros";
+  $error_201_causa="Error de Parámetros";
+
+  $error_202_mensaje="Usuario no registrado";
+  $error_202_causa="Error de Autenticación";
+
+  $error_203_mensaje="Contraseña incorrecta";
+  $error_203_causa="Error de Autenticación";
+
+  $error_204_mensaje="El registro ya existe";
+  $error_204_causa="Error de registro";
+
+  $error_205_mensaje="Código caducado";
+  $error_205_causa="Error de código";
+
+  $error_206_mensaje="Código incorrecto";
+  $error_206_causa="Error de código";
+
+  $error_207_mensaje="No existen categorías activas";
+  $error_207_causa="Error de categoría";
+
+  $error_208_mensaje="No existe contacto ";
+  $error_208_causa="Error de contacto ";
+
+  $error_209_mensaje="El asiento no se encuentra disponible";
+  $error_209_causa="Asiento no disponible ";
+
+  $error_210_mensaje="Aforo de platea no disponible";
+  $error_210_causa="Asientos no disponible ";
+
+  $error_211_mensaje="Aforo de función no disponible";
+  $error_211_causa="Asientos no disponible ";
+
+  $error_212_mensaje="La reserva de asiento ya caducó";
+  $error_212_causa="Reserva caducó";
+
+  $error_213_mensaje="Parámetros de evento no son los correctos";
+  $error_213_causa="Error de Parámetros";
+
+  $error_214_mensaje="Tipo de evento incorrecto";
+  $error_214_causa="Error de Parámetros";
+
+  $error_215_mensaje="Sub total no esta correcto";
+  $error_215_causa="Error de Parámetros";
+
+  $error_216_mensaje="Descuento no esta correcto";
+  $error_216_causa="Error de Parámetros";
+
+  $error_217_mensaje="Iva Incorrecto";
+  $error_217_causa="Error de Parámetros";
+
+  $error_218_mensaje="Valores incorrectos";
+  $error_218_causa="Error de Parámetros";
+
+  $error_219_mensaje="Error de Transacción";
+  $error_219_causa="Ingrese otra tarjeta";
+
+  $error_220_mensaje="Tipo de promocion no existe";
+  $error_220_causa="Error de Parámetros";
+
+  $error_221_mensaje="Error de Transacción";
+  $error_221_causa="Transacción no es exitosa";
+
+  $error_222_mensaje="Usuario Bloqueado";
+  $error_222_causa="Contactarse con Administración del Teatro";
+
+  $error_223_mensaje="Correo no verificado";
+  $error_223_causa="Favor verificar correo";
+
+  $error_224_mensaje="Cantidad de puntos no válida";
+  $error_224_causa="Cantidad de Puntos acumulados no disponible";
+
+  $error_225_mensaje="Código Incorrecto";
+  $error_225_causa="Ingresé Qr Válido";
+
+  $error_226_mensaje="Asientos incorrectos";
+  $error_226_causa="Error de asientos";
+
+  $error_228_mensaje="Función incorrecta";
+  $error_228_causa="Error de Función";
+
+  $error_227_mensaje="No cuenta con asientos disponible";
+  $error_227_causa="Error de Asientos";
+?>
