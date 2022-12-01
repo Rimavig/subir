2aa524bdb7445446459d966d7eef39693978f91b
Rimavig <rvivanco@espol.edu.ec>
2022-12-01 12:25:53 -0500

    gg

---
+++ sorteo_guayaquil/totem.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Totem</title>
+</head>
+<body>
+    <img src="images/fondo-employee.png" class="backgorund-image-employee" alt="" srcset="">
+    <div class="linea-fondo d-flex flex-row cliente">
+      <div class="d-flex flex-column width-100">
+
+          <div class="d-flex flex-column">
+              <div class="margin-top-1"></div>
+              <h1 class="winner-name2" id="names-winner">LA FIESTA DEL AÑO</h1>
+              <div class="margin-top-1"></div>
+              <h1 class="info-user2">DESPUÉS DE UN LARGO TIEMPO VOLVEMOS A CELEBRAR JUNTOS</h1>
+          </div>
+      </div>
+
+    </div>
+</body>
+<script src="jquery.min.js"></script>
+<script src="totem.js"></script>
+</html>
